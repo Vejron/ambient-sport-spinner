b0149e106db3e2462504d8ac43ff01ae7933da86
Vejron <bjorn.yttergren@gmail.com>
2016-06-13 16:38:43 +0200

    sdf

--- ass-web/app.js
+++ ass-web/app.js
@@ -3,10 +3,14 @@ Thing = new Mongo.Collection("thing");
 if (Meteor.isClient) {
     // counter starts at 0
     Session.setDefault('counter', 0);
+    Session.setDefault('spinAnimation', 'stop');
 
     Template.body.helpers({
         counter: function() {
             return Session.get('counter');
+        },
+        spinAnimation: function () {
+            return Session.get('spinAnimation');
         }
     });
 
@@ -14,6 +18,7 @@ if (Meteor.isClient) {
         'click button': function() {
             // increment the counter when button is clicked
             Session.set('counter', Session.get('counter') + 1);
+            Session.set('spinAnimation', 'crank-slow');
             // Make the thing spin
             Meteor.call('spin', {frames: 6, delay: 200});
         }
@@ -22,6 +27,7 @@ if (Meteor.isClient) {
         'click button': function() {
             // increment the counter when button is clicked
             Session.set('counter', Session.get('counter') + 1);
+            Session.set('spinAnimation', 'crank-medium');
             // Make the thing spin
             Meteor.call('spin', {frames: 12, delay: 60});
         }
@@ -30,8 +36,16 @@ if (Meteor.isClient) {
         'click button': function() {
             // increment the counter when button is clicked
             Session.set('counter', Session.get('counter') + 1);
+            Session.set('spinAnimation', 'crank-insane');
             // Make the thing spin
             Meteor.call('spin', {frames: 24, delay: 100});
+        }
+    });
+    Template.stop.events({
+        'click button': function() {
+            Session.set('spinAnimation', 'stop');
+            // Make the thing spin
+            //Meteor.call('spin', {frames: 24, delay: 100});
         }
     });
 }
@@ -41,7 +55,7 @@ if (Meteor.isServer) {
         // code to run on server at startup
 
         //connect to broker and subscribe to status message
-        Thing.mqttConnect('mqtt://test.mosquitto.org', ['fredrik40/beat/'], {
+        Thing.mqttConnect('mqtt://test.mosquitto.org', ['cici40/beat/'], {
             insert: false,
             raw: false
         });
@@ -49,7 +63,7 @@ if (Meteor.isServer) {
     
     Meteor.methods({
     'spin': function(doc) {
-      Thing.insert({ topic: 'fredrik40/run/', message: JSON.stringify(doc), broadcast: true });
+      Thing.insert({ topic: 'cici40/run/', message: JSON.stringify(doc), broadcast: true });
       console.log(Thing.find().fetch());
     },
     'other': function() {
